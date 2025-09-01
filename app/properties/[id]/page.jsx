74641803864402f2921d5af9c6269509890d4f96
Andrei Varcus <andrei.varcus@yahoo.com>
2025-09-01 23:49:41 +0300

    finished main part of property details

--- app/properties/[id]/page.jsx
+++ app/properties/[id]/page.jsx
@@ -1,5 +1,37 @@
-const PropertyPage = ({ params }) => {
-    return <div>Property Page {params.id}</div>;
+import PropertyHeaderImage from '@/components/PropertyHeaderImage';
+import PropertyDetails from '@/components/PropertyDetails';
+import connectDB from '@/config/database';
+import Property from '@/models/Property';
+import Link from 'next/link';
+import { FaArrowLeft } from 'react-icons/fa';
+
+const PropertyPage = async ({ params }) => {
+    await connectDB();
+    const property = await Property.findById(params.id).lean();
+
+    return (
+        <>
+            <PropertyHeaderImage image={property.images[0]} />
+            {/* <!-- Go Back --> */}
+            <section>
+                <div className="container m-auto py-6 px-6">
+                    <Link
+                        href="/properties"
+                        className="text-blue-500 hover:text-blue-600 flex items-center"
+                    >
+                        <FaArrowLeft className="mr-2" /> Back to Properties
+                    </Link>
+                </div>
+            </section>
+            <section class="bg-blue-50">
+                <div class="container m-auto py-10 px-6">
+                    <div class="grid grid-cols-1 md:grid-cols-70-30 w-full gap-6">
+                        <PropertyDetails property={property} />
+                    </div>
+                </div>
+            </section>
+        </>
+    );
 };
 
 export default PropertyPage;
--- components/HomeProperties.jsx
+++ components/HomeProperties.jsx
@@ -1,9 +1,16 @@
-import properties from '@/properties.json';
 import PropertyCard from './PropertyCard';
 import Link from 'next/link';
+import connectDB from '@/config/database';
+import Property from '@/models/Property';
 
-const HomeProperties = () => {
-    const recentProperties = properties.slice(0, 3);
+const HomeProperties = async () => {
+    await connectDB();
+
+    const recentProperties = await Property.find({})
+        .sort({ createdAt: -1 })
+        .limit(3)
+        .lean();
+
     return (
         <>
             <section className="px-4 py-6">
@@ -11,7 +18,7 @@ const HomeProperties = () => {
                     <h2 className="text-3xl font-bold text-blue-500 mb-6 text-center">
                         Recent Properties
                     </h2>
-                    {properties.length === 0 ? (
+                    {recentProperties.length === 0 ? (
                         <p>No properties found</p>
                     ) : (
                         <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
