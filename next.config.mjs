fc46be28421d0caf068da2fcc189ebf2a33a627a
Andrei Varcus <andrei.varcus@yahoo.com>
2025-09-03 18:15:24 +0300

    finished formdata action & stated getSessionUser

--- next.config.mjs
+++ next.config.mjs
@@ -9,6 +9,11 @@ const nextConfig = {
             },
         ],
     },
+    experimental: {
+        serverActions: {
+            bodySizeLimit: '3mb',
+        },
+    },
 };
 
 export default nextConfig;
