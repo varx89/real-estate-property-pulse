47adaff521472eab42589bc264997bfd9c116667
Andrei Varcus <andrei.varcus@yahoo.com>
2025-09-03 22:05:28 +0300

    finished add properties to db tested and works

--- app/actions/addProperty.js
+++ app/actions/addProperty.js
@@ -1,6 +1,21 @@
 'use server';
+import connectDB from '@/config/database';
+import Property from '@/models/Property';
+import { getSessionUser } from '@/utils/getSessionUser';
+import { revalidatePath } from 'next/cache';
+import { redirect } from 'next/navigation';
 
 async function addProperty(formData) {
+    await connectDB();
+
+    const sessionUser = await getSessionUser();
+
+    if (!sessionUser || !sessionUser.userId) {
+        throw new Error('User ID is required');
+    }
+
+    const { userId } = sessionUser;
+
     // get all amenities
     const amenities = formData.getAll('amenities');
     const images = formData
@@ -9,6 +24,7 @@ async function addProperty(formData) {
         .map((image) => image.name);
 
     const propertyData = {
+        owner: userId,
         type: formData.get('type'),
         name: formData.get('name'),
         description: formData.get('description'),
@@ -34,6 +50,11 @@ async function addProperty(formData) {
         },
         images,
     };
-    console.log(propertyData);
+    const newProperty = new Property(propertyData);
+    await newProperty.save();
+
+    revalidatePath('/', 'layout');
+
+    redirect(`/properties/${newProperty._id}`);
 }
 export default addProperty;
